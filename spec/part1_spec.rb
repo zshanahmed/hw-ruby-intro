b968d2157c260f0c520d2aba57c47de5c486bcae
Hans Johnson <hans-johnson@uiowa.edu>
2020-09-02 14:20:16 -0500

    ENH: Student accesible rspec files outdated.

--- spec/part1_spec.rb
+++ spec/part1_spec.rb
@@ -13,6 +13,7 @@ describe 'Ruby intro part 1' do
       expect(sum([1, 2, 3, 4, 5])).to eq(15)
       expect(sum([1, 2, 3, 4, -5])).to eq(5)
       expect(sum([1, 2, 3, 4, -5, 5, -100])).to eq(-90)
+      expect(sum([-1])).to eq(-1)
     end
 
     it 'works on the empty array [10 points]', points: 10 do
@@ -21,35 +22,38 @@ describe 'Ruby intro part 1' do
     end
   end
 
-  describe '#max_2_sum' do
-    it 'is defined' do
-      expect { max_2_sum([1, 2, 3]) }.not_to raise_error
+  describe '#even_product' do
+    it 'must be defined' do
+      expect { even_product([1, 2, 3]) }.not_to raise_error
     end
-    it 'returns the correct sum [7 points]', points: 7 do
-      expect(max_2_sum([1, 2, 3, 4, 5])).to be_a_kind_of Integer
-      expect(max_2_sum([1, -2, -3, -4, -5])).to eq(-1)
+    it 'returns the correct product [7 points]', points: 7 do
+      expect(even_product([1, 2, 3, 4, 5])).to be_a_kind_of Integer
+      expect(even_product([1, -2, -3, -4, -5])).to eq(8)
     end
-    it 'works even if 2 largest values are the same [3 points]', points: 3 do
-      expect(max_2_sum([1, 2, 3, 3])).to eq(6)
+    # Test case no longer applicable, substituted with the immediately proceeding case
+    # it 'works even if 2 largest values are the same [3 points]', points: 3 do
+    #   expect(even_product([1, 2, 3, 3])).to eq(9)
+    # end
+    it 'returns zero when no even numbers are present [3 points]', points: 3 do
+      expect(even_product([1, 7, 3, 3])).to be_zero
     end
     it 'returns zero if array is empty [10 points]', points: 10 do
-      expect(max_2_sum([])).to be_zero
+      expect(even_product([])).to be_zero
     end
     it 'returns value of the element if just one element [10 points]', points: 10 do
-      expect(max_2_sum([3])).to eq(3)
+      expect(even_product([2])).to eq(2)
     end
   end
 
-  describe '#sum_to_n' do
-    it 'is defined' do
-      expect { sum_to_n?([1, 2, 3], 4) }.not_to raise_error
+  describe '#abs_difference_is_n?' do
+    it 'must be defined' do
+      expect { abs_difference_is_n?([1, 2, 3], 4) }.not_to raise_error
     end
     it 'returns true when any two elements sum to the second argument [30 points]', points: 30 do
-      expect(sum_to_n?([1, 2, 3, 4, 5], 5)).to be true # 2 + 3 = 5
-      expect(sum_to_n?([3, 0, 5], 5)).to be true # 0 + 5 = 5
-      expect(sum_to_n?([-1, -2, 3, 4, 5, -8], -3)).to be true  # handles negative sum
-      expect(sum_to_n?([-1, -2, 3, 4, 5, -8], 12)).to be false # 3 + 4 + 5 = 12 (not 3 elements)
-      expect(sum_to_n?([-1, -2, 3, 4, 6, -8], 12)).to be false # no two elements that sum
+      expect(abs_difference_is_n?([1, 2, 3, 4, 5], 4)).to be true
+      expect(abs_difference_is_n?([3, 0, 5], 5)).to be true
+      expect(abs_difference_is_n?([-1, -2, 3, 4, 5, -8], -12)).to be false
+      expect(abs_difference_is_n?([-1, -2, 3, 4, 6, -8], -12)).to be false
     end
     #    for rspec 2.14.1
     # it 'returns false for the single element array [5 points]', points: 5 do
@@ -61,14 +65,12 @@ describe 'Ruby intro part 1' do
     #   sum_to_n?([], 7).should be_false
     # end
     it 'returns false for any single element array [5 points]', points: 5 do
-      expect(sum_to_n?([0], 0)).to be false
-      expect(sum_to_n?([1], 1)).to be false
-      expect(sum_to_n?([-1], -1)).to be false
-      expect(sum_to_n?([-3], 0)).to be false
+      expect(abs_difference_is_n?([1], 1)).to be false
+      expect(abs_difference_is_n?([3], 0)).to be false
     end
     it 'returns false for an empty array [5 points]', points: 5 do
-      expect(sum_to_n?([], 0)).to be false
-      expect(sum_to_n?([], 7)).to be false
+      expect(abs_difference_is_n?([], 0)).to be false
+      expect(abs_difference_is_n?([], 7)).to be false
     end
   end
 end
--- spec/part2_spec.rb
+++ spec/part2_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../lib/ruby_intro'
-
 describe '#hello' do
   it 'is defined' do
     expect { hello('Testing') }.not_to raise_error #::NoMethodError)
@@ -9,11 +8,15 @@ describe '#hello' do
 
   it 'The hello method returns the correct string [30 points]', points: 30 do
     expect(hello('Dan').class).to eq(String)
-    expect(hello('Dan')).to eq('Hello, Dan'), 'Incorrect results for input: "Dan"'
-    expect(hello('BILL')).to eq('Hello, BILL'), 'Incorrect results for input: "BILL"'
-    expect(hello('Mr. Ilson')).to eq('Hello, Mr. Ilson'), 'Incorrect results for input: "Mr. Ilson"'
+    expect(hello('Dan')).to eq('Hello, Dan your name has 3 characters in it.'),
+                            'Incorrect results for input: "Dan"'
+    expect(hello('BILL')).to eq('Hello, BILL your name has 4 characters in it.'),
+                             'Incorrect results for input: "BILL"'
+    expect(hello('Mr. Ilson')).to eq('Hello, Mr. Ilson your name has 9 characters in it.'),
+                                  'Incorrect results for input: "Mr. Ilson"'
   end
 end
+
 
 describe '#starts_with_consonant?' do
   it 'is defined' do
@@ -43,16 +46,16 @@ describe '#binary_multiple_of_4?' do
   it 'is defined' do
     expect { binary_multiple_of_4?('yes') }.not_to raise_error #::NoMethodError)
   end
-  it 'classifies valid binary numbers [30 points]', points: 30 do
-    %w[1010101010100 0101010101010100 100 0].each do |string|
-      expect(binary_multiple_of_4?(string)).to be_truthy, "Incorrect results for input: \"#{string}\""
+  it 'classifies valid binary numbers returning boolean values [10 points]', points: 1 do
+    %w[1010101010100 0101010101010100 100 0 00000000].each do |string|
+      expect(binary_multiple_of_4?(string)).to be(true), "Incorrect results for input: \"#{string}\""
     end
-    %w[101 1000000000001].each do |string|
-      expect(binary_multiple_of_4?(string)).not_to be_truthy, "Incorrect results for input: \"#{string}\""
+    %w[101 1000000000001 00000010 010101010].each do |string|
+      expect(binary_multiple_of_4?(string)).to be(false), "Incorrect results for input: \"#{string}\""
     end
   end
-  it 'rejects invalid binary numbers [10 points]', points: 10 do
-    expect(binary_multiple_of_4?('a100')).to be_falsy, "'a100' is not a valid binary number!"
-    expect(binary_multiple_of_4?('')).to be_falsy, 'The empty string is not a valid binary number!'
+  it 'rejects invalid binary numbers with true/false [5 points]', points: 1 do
+    expect(binary_multiple_of_4?('a100')).to be(false), "'a100' is not a valid binary number!"
+    expect(binary_multiple_of_4?('')).to be(false), 'The empty string is not a valid binary number!'
   end
 end
